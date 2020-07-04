2ceb264334d1fac03031a26063c01885e46da44c
Khrystyna  Fedyuk <k.fedyuk@campus.fct.unl.pt>
2020-07-04 19:19:39 +0100

    Adds google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,5 +31,14 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-171613972-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+      },
+    },
   ],
 }
